b9d713bb6b39c1c2aa1d1ff159ec83ff2707ac67
Sage Lee <sagemcklee@gmail.com>
2020-08-01 20:59:22 -0600

    Beginnings of forecast spec

---
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'MapQuest API' do 
+    it "sends latitude and longitude for specified city" do 
+    
+      get "/api/v1/forecast?location=denver,co"
+
+      expect(response).to be_successful  
+
+      forecast = JSON.parse(response.body)
+      expect(forecast).to eq ( )
+     
+
+    end 
+end 
