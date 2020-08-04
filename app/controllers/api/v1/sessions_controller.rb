599ef69cd57e87b14bde1252aacb16f371dec047
Sage Lee <sagemcklee@gmail.com>
2020-08-04 14:30:43 -0600

    Add log in sad path

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:email])
-        binding.pry
          if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             render json: UserSerializer.new(user), status: 200
-        else
-            render json: {errors: user.errors.full_messages, status: 400}           
+         elsif user == nil
+            render json:{errors: "Invalid Credentials", status: 400} 
+         else
+            render json: {errors: "Incorrect Password", status: 400}           
         end
     end 
 end 
--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -1,17 +1,8 @@
 require 'rails_helper'
 
-describe 'Session Serializer' do 
-  it "returns image url" do
-
-        post "/api/v1/sessions", params: {
-        "email": "whatever@example.com",
-        "password": "password"
-        }
-    expect(response).to be_successful  
-    expect(response.status).to eq(200)     
-    session = JSON.parse(response.body, symbolize_names: true)
-
-     post "/api/v1/users", params: {
+describe 'Login Request' do 
+  it "sucessful log in request returns user info and api key" do
+         post "/api/v1/users", params: {
       "email": "whatever@example.com",
       "password": "password",
       "password_confirmation": "password"
@@ -24,18 +15,33 @@ describe 'Session Serializer' do
     expect(response).to be_successful  
     expect(response.status).to eq(200)     
     session = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
-
-   # session_status = JSON.parse(status, symbolize_names: true)
-    expect(session).to eq({
-  "data": {
-    "type": "users",
-    "id": "1",
-    "attributes": {
-      "email": "whatever@example.com",
-      "api_key": "jgn983hy48thw9begh98h4539h4"
-    }
-  }
-})   
+    expect(session[:data][:type]).to eq("user")
+    expect(session[:data][:id]).to_not be_empty
+    expect(session[:data][:attributes][:email]).to eq("whatever@example.com")
+    expect(session[:data][:attributes][:api_key]).to_not be_empty 
   end 
+  it "incorrect password in log in request returns incorrect password" do
+             post "/api/v1/users", params: {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+  
+    post "/api/v1/sessions", params: {
+        "email": "whatever@example.com",
+        "password": "passwor"
+        }  
+    session = JSON.parse(response.body, symbolize_names: true)
+    expect(session).to eq({:errors=>"Incorrect Password", :status=>400})     
+
+  end 
+  it "incorrect email in log in request returns invalid credentials error " do
+
+        post "/api/v1/sessions", params: {
+        "email": "whatever@example.com",
+        "password": "password"
+        }
+    session = JSON.parse(response.body, symbolize_names: true)
+    expect(session).to eq({:errors=>"Invalid Credentials", :status=>400})
+  end     
 end 
