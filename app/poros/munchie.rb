1752bed2b5780dadefcb6b34a93bed4698647853
Sage Lee <sagemcklee@gmail.com>
2020-08-04 11:14:30 -0600

    Add destination_forecast extension

--- app/poros/munchie.rb
+++ app/poros/munchie.rb
@@ -1,12 +1,12 @@
 class Munchie
     attr_reader :start_location, :end_location, :travel_time, :destination_forecast, :restaurant, :id, :travel_time
-    def initialize(start, destination, arrival_forecast, term)
+    def initialize(start, destination, current_forecast, arrival_forecast, term)
         @food = FoodService.new
         @map = MapService.new
         @id = "null"
         @start_location = start
         @end_location = destination
-        @destination_forecast = arrival_forecast
+        @destination_forecast = {"current": current_forecast, "arrival": arrival_forecast}
         @arrival_time_in_secs = @map.arrival_time(start, destination)
         @restaurant = @food.yelp(destination, term, @arrival_time_secs)
         @travel_time = @map.travel_time(start, destination)
