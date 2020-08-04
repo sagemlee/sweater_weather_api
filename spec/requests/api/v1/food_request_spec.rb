3aebb510b63eccc9a17d9da2b12d62c20cac201d
Sage Lee <sagemcklee@gmail.com>
2020-08-04 11:58:02 -0600

    Start of sad path testing

--- spec/requests/api/v1/food_request_spec.rb
+++ spec/requests/api/v1/food_request_spec.rb
@@ -10,7 +10,7 @@ describe 'Food Endpoint' do
     }
     expect(response).to be_successful  
     food = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
+    
     expect(food[:data][:id]).to eq("null")
     expect(food[:data][:type]).to eq("munchie")
     expect(food[:data][:attributes][:start_location]).to eq("Denver,CO")
@@ -20,25 +20,15 @@ describe 'Food Endpoint' do
     expect(food[:data][:attributes][:destination_forecast][:current]).to_not be_empty
     expect(food[:data][:attributes][:restaurant][:name]).to_not be_empty
     expect(food[:data][:attributes][:restaurant][:address]).to_not be_empty
-    binding.pry    
-    expect(food[:data][:attributes]).to eq({
-  "data": {
-    "id": "null",
-    "type": "munchie",
-    "attributes": {
-      "start_location": "Denver, CO",
-      "end_location": "Pueblo, CO",
-      "travel_time": "1 hours 48 min",
-      "destination_forecast": {
-        "current": "Cloudy with a chance of meatballs",
-        "arrival": "Rain with a pastability of hail"
-      },
-      "restaurant": {
-        "name": "All Night Asian Food",
-        "address": "1234 Main St, Unit 12, Pueblo, CO 81009"
-      }
-    }
-  }
-})     
+    
+    binding.pry  
+   
+#   get "/api/v1/munchies", params: {
+#       "start": "denver,co",
+#       "food": "chinese"
+#     }  
+
+#     bad_food = JSON.parse(response.body, symbolize_names: true)
+ 
   end 
 end 
