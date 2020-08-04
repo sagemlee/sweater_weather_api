e1051a3f2d5d72c94f6855018f7c6e48ba649792
Sage Lee <sagemcklee@gmail.com>
2020-08-04 10:58:19 -0600

    Add travel time extension

--- app/poros/munchie.rb
+++ app/poros/munchie.rb
@@ -1,10 +1,11 @@
 class Munchie
-    attr_reader :start_location, :end_location, :travel_time, :destination_forecast, :restaurant, :id
+    attr_reader :start_location, :end_location, :travel_time, :destination_forecast, :restaurant, :id, :travel_time
     def initialize(start, destination, arrival_forecast, term)
         @id = "null"
         @start_location = start
         @end_location = destination
         @destination_forecast = arrival_forecast
         @restaurant = FoodService.new.yelp(destination, term)
+        @travel_time = MapService.new.travel_time(start, destination)
     end 
 end 
